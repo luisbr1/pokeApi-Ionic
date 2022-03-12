4045d58d163804ab65b250438db4b91f9c5e82f5
Luis.Paes <79490376+luisbr1@users.noreply.github.com>
2022-03-11 22:23:09 -0300

    using arrays

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  urlImg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+  listaPokemon = [
+    {numero: 1, nome: 'Bulbasaur', tipos: ['grass', 'poison'], foto:'001.png'},
+    {numero: 4, nome: 'Charmander', tipos: ['fire'], foto:'004.png'},
+    {numero: 7, nome: 'Squirtle', tipos: ['water'], foto:'007.png'},
+    {numero: 25, nome: 'Pikachu', tipos: ['eletric'], foto:'025.png'},
+    {numero: 149, nome: 'Dragonite', tipos: ['dragon', 'flying'], foto:'149.png'},
+    
+  ];
+
   constructor() {}
 
 }
